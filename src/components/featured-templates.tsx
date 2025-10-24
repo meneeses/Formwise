163d9bcce28e7f75590bef0d68cec04cce0bab64
João Meneses <meneses-joao@hotmail.com>
2025-10-24 12:21:35 -0300

    feat: v2

--- src/components/featured-templates.tsx
+++
@@ -1,7 +0,0 @@
-import { templates } from "@/lib/data";
-import TemplateGrid from "./ui/TemplateGrid";
-
-export default function FeaturedTemplates() {
-  const featured = templates.slice(0, 6);
-  return <TemplateGrid items={featured} />;
-}
